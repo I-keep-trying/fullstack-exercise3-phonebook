0304d4c1e44e004fb78dd62fec986579b52bd1b4
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2020-08-03 16:40:59 -0400

    exercises 3.13 and 3.14

--- index.js
+++ index.js
@@ -1,66 +1,40 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const Person = require('./models/people')
 
 app.use(express.json())
 app.use(express.static('build'))
 
-morgan.token('person', (request) => {
-    return JSON.stringify(request.body)
+morgan.token('person', request => {
+  return JSON.stringify(request.body)
 })
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
-
-let persons = [
-  {
-    name: 'Arto Hellas',
-    number: '040-123456',
-    id: 1,
-  },
-  {
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-    id: 2,
-  },
-  {
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-    id: 3,
-  },
-  {
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-    id: 4,
-  },
-]
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :person'
+  )
+)
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/info', (req, res) => {
-  const date = new Date()
-  res.send(
-    `<h1>Phonebook has info for ${persons.length} people </h1> <br /> <h3> ${date}</h3> `
-  )
-})
-
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 // I found (thanks to stackoverflow) a way to guarantee no duplicate id's are created.
-const generateId = () => {
+/* const generateId = () => {
   let randoms = []
   while (randoms.length < persons.length + 1) {
     let id = Math.ceil(1 + Math.floor(Math.random() * 100))
@@ -69,34 +43,25 @@ const generateId = () => {
       return id
     }
   }
-}
+} */
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  if (!body.name || !body.number) {
+
+  if (body.name === undefined || body.number === undefined) {
     return response.status(400).json({
       error: 'name or number missing',
     })
   }
 
-  if (
-    persons.find(
-      person => person.name.toLowerCase() === body.name.toLowerCase()
-    )
-  ) {
-    return response.status(401).json({
-      error: 'duplicate exists',
-    })
-  }
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  }
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -106,7 +71,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3008
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -5,12 +5,12 @@ const password = process.argv[2]
 const url = `mongodb+srv://fullstack2020:${password}@cluster0-lw40o.mongodb.net/phonebook-app?retryWrites=true`
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
-const phonebookSchema = new mongoose.Schema({
+/* const phonebookSchema = new mongoose.Schema({
   name: String,
   number: String,
-})
+}) */
 
-const Person = mongoose.model('Person', phonebookSchema)
+//const Person = mongoose.model('Person', phonebookSchema)
 
 const name = process.argv[3]
 const number = process.argv[4]
