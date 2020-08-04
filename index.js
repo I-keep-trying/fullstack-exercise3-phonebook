9261a2174dbce3b4306de235c0f03c869c072c60
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2020-08-04 14:59:20 -0400

    exercise 3.17

--- index.js
+++ index.js
@@ -27,10 +27,16 @@ app.get('/api/persons', (req, res) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 // I found (thanks to stackoverflow) a way to guarantee no duplicate id's are created.
@@ -47,8 +53,13 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-console.log('post body', body)
-  if (body.name === undefined || body.number === undefined || body.name.length === 0 || body.number.length === 0) {
+  console.log('post body', body)
+  if (
+    body.name === undefined ||
+    body.number === undefined ||
+    body.name.length === 0 ||
+    body.number.length === 0
+  ) {
     return response.status(400).json({
       error: 'name or number missing',
     })
@@ -64,11 +75,43 @@ console.log('post body', body)
   })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 app.delete('/api/persons/:id', async (request, response) => {
   await Person.findByIdAndRemove(request.params.id)
   response.status(204).end()
-
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
